657a52bb8855676a7ab03471e54f1f856751a0c5
Trần Thanh Hoà <82800890+thanhhoa2002@users.noreply.github.com>
2022-12-23 02:11:36 +0700

    update User Page UI

--- src/pages/AccountPage/index.js
+++ src/pages/AccountPage/index.js
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux';
 import {selectUser} from '../../features/userSlice'
 import  Axios  from 'axios';
 import * as React from 'react';
-import './style.css';
+import './style.scss';
 
 function AccountPage() {
 
@@ -23,20 +23,40 @@ function AccountPage() {
 
     return <>
 
-        <div className='container'>
-            <h1 className='title-postpage'>My account</h1>
+        <div className='container text-center'>
+            <br/>
+            <div className="row justify-content-center">
+                <div className="col-md-5">
+                    <div className="wrapper">
+                        <div className="row no-gutters">
+                                <div className="user-section rounded-5">
+                                    <h1>PROFILE PAGE</h1>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+            
             {/* <img src={inforUser.avatar} alt="#" style={{width:'30%'}}/> */}
             {/* <h1>{inforUser.email}</h1>
             <img src={inforUser.avatar} alt="harry potter" style={{ width: '400px', }}/> */}
-            <div className="card">
-                <img src={inforUser.avatar} alt="#" style={{width:'100%'}}/>
-                <h1>{inforUser.username}</h1>
-                <p className="title">{inforUser.firstName} {inforUser.lastName}</p>
-                <p>{inforUser.email}</p>
-                <p>{inforUser.phoneNumber}</p>
+            <br/>
+            <div className="row justify-content-center">
+                <div className="col-md-5">
+                    <div className="wrapper">
+                        <div className="row no-gutters">
+                                <div className="user-wrap w-100 p-lg-5 p-4 rounded-5">
+                                <img className='avar rounded-circle' src={inforUser.avatar} alt="#" />
+                                <h3>{inforUser.username}</h3>
+                                <p className="title">{inforUser.firstName} {inforUser.lastName}</p>
+                                <p className='mail'>{inforUser.email}</p>
+                                <p className='phone'>{inforUser.phoneNumber}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        
     </>;
 }
 
